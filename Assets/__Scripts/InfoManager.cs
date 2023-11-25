8bf33d0b51723c17d257f187cdec198736ccd975
wzeng0 <wzeng00@uchicago.edu>
2023-11-25 16:47:47 -0600

    initialized script to toggle between character dialogue

---
+++ Assets/__Scripts/InfoManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InfoManager : MonoBehaviour
+{
+    public TMP_Text ClientDialogue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomNumber = Random.Range(0, 9);
+    }
+}
